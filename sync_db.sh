f1b9a20e0e1272b99bee356fd7d130eeff03e65c
Viktor Koop <viktor.koop@gmail.com>
2015-03-09 18:19:07 +0100

    combine restore and sync scripts

--- sync_db.sh
+++ sync_db.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 OPTIND=1
-optspec=":h-:"
+optspec=":h-:d:"
 while getopts "$optspec" optchar; do
 	case "${optchar}" in
 		h )
 			echo 'help...'
 			exit 0
+			;;
+		d)
+			echo 'selected'
+        		RESTORE_DAY=$OPTARG
 			;;
 		- )
 			case "${OPTARG}" in
@@ -18,6 +22,10 @@ while getopts "$optspec" optchar; do
 					echo 'Sync server to local.';
 					DIRECTION='stol';
 					;;		
+				btos)
+					echo 'Restore DB from backup.';
+					DIRECTION='btol';
+					;;
 				*)
 					echo "Unknown option";
 					exit 0
@@ -35,11 +43,17 @@ source "functions.sh"
 
 DUMPCOMMAND=(mysqldump -u $DB_USERNAME -p$DB_PASSWORD $DB_NAME)
 
-IMPORTCOMMAND=(mysql -u${DB_USERNAME} -p${DB_PASSWORD} $DB_NAME --host=127.0.0.1)
+IMPORTCOMMAND=(mysql -u$DB_USERNAME -p$DB_PASSWORD $DB_NAME --host=127.0.0.1)
 
 if [[ $DIRECTION == 'ltos' ]]; then
 	eval "${DUMPCOMMAND[@]}" | ssh $SSHOPT $SERVERHOST "${IMPORTCOMMAND[@]}"
+elif [[ $DIRECTION == 'btos' ]]; then
+	SRC="${BASEBACKUPFOLDER}/${SERVERNAME}/DB/$RESTORE_DAY.sql"
+	SQLCOMMAND="mysql --host=127.0.0.1 -u${DB_USERNAME} -p${DB_PASSWORD} ${DB_NAME}"
+
+	cat $SRC | ssh $SSHOPT $SERVERHOST $SQLCOMMAND
 else
+	#statements
 	ssh $SSHOPT $SERVERHOST "${DUMPCOMMAND[@]}" | eval "${DUMPCOMMAND[@]}"
 fi
 
