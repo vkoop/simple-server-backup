487b61c6d76b4a07d48b65e69f11f28d525cba09
Viktor Koop <viktor.koop@gmail.com>
2015-03-12 20:22:36 +0100

    cleanup

--- backup.sh
+++ backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 BASEDIR=$(dirname $0)
 
-source ${BASEDIR}/backup_db.sh $1
-source ${BASEDIR}/backup_server.sh $!
+source $BASEDIR/backup_db.sh $1
+source $BASEDIR/backup_server.sh $!
 
 echo "Backup finished " $(date)
