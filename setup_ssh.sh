fd47e2b2e65183ed9854ebab91fd6eb64deffe83
Viktor Koop <viktor.koop@gmail.com>
2017-07-29 00:25:16 +0200

    create known hosts file

--- setup_ssh.sh
+++ setup_ssh.sh
@@ -7,6 +7,11 @@ do
   echo "Setup ssh server: ${f}";
   source $f;
 
+  if [ ! -f "~/.ssh/known_hosts" ]; then
+    mkdir -p ~/.ssh
+    touch ~/.ssh/known_hosts
+  fi
+
   ssh-keyscan -H $SERVERHOST >> ~/.ssh/known_hosts
 
   echo "----------------------------------";
