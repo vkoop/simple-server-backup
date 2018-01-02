b6ea06841aea79149f8c4dcc07fcf7ea0bf3d879
Viktor Koop <viktor.koop@gmail.com>
2018-01-02 15:04:35 +0100

    only define ssh port if not already set

--- functions.sh
+++ functions.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # T: turn off pseudo-tty to decrease cpu load on destination.
-# c arcfour: use the weakest but fastest SSH encryption. Must specify "Ciphers arcfour" in sshd_config on destination.
 # o Compression=no: Turn off SSH compression.
 # x: turn off X forwarding if it is on by default.
-SSHOPT="-i ${SSHPASSFILE} -l ${SSHUSERNAME} -p 22 -T -o Compression=no -x"
+
+SSHPORT="${SSHPORT:-22}"
+SSHOPT="-i ${SSHPASSFILE} -l ${SSHUSERNAME} -p ${SSHPORT} -T -o Compression=no -x"
