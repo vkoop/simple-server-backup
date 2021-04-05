d4caf859d99a3fd10e95e104c02910009fa98ad1
Viktor Koop <viktor.koop@googlemail.com>
2021-04-05 10:00:52 +0200

    quotes

--- backup_all_servers.sh
+++ backup_all_servers.sh
@@ -5,6 +5,6 @@ BASEDIR=$(dirname $0)
 for f in $BASEDIR/config/*.config.sh
 do
   echo "Starting backup with config: ${f}";
-  source $BASEDIR/backup.sh $f;
+  source $BASEDIR/backup.sh "$f";
   echo "----------------------------------";
 done
