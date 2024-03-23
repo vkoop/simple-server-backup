76c9942e6dce2ffa2e2633d7a2647910bd80e145
Viktor Koop <viktor.koop@gmail.com>
2024-03-23 09:05:01 +0100

    Refactor sync_data.sh script to improve readability and add usage information

--- sync_data.sh
+++ sync_data.sh
@@ -5,26 +5,35 @@ optspec=":h-:d:"
 while getopts "$optspec" optchar; do
 	case "${optchar}" in
 		h )
-			echo 'help...'
+			echo 'Usage: sync_data.sh [OPTIONS] CONFIG'
+			echo 'Synchronize data between local and remote servers.'
+			echo
+			echo 'Options:'
+			echo '  -h        Show this help message and exit.'
+			echo '  -d DATE   Specify the date for restoring from backup (format: yyyy-mm-dd).'
+			echo '  --ltos    Sync local to server.'
+			echo '  --stol    Sync server to local.'
+			echo '  --btos    Restore from backup.'
+			echo
+			echo 'CONFIG is the path to the configuration file.'
 			exit 0
 			;;
 		d )
-			echo 'selected'
-        		DATE=$OPTARG
+        	DATE=$OPTARG
 			;;
 		- )
 			case "${OPTARG}" in
 				ltos)
 					echo 'Sync local to server.';
-					DIRECTION='ltos'
+					SYNC_DIRECTION='ltos'
 					;;
 				stol)
 					echo 'Sync server to local.';
-					DIRECTION='stol';
+					SYNC_DIRECTION='stol';
 					;;
                 btos)
 					echo 'Restore from backup.';
-					DIRECTION='btos';
+					SYNC_DIRECTION='btos';
 					;;
 				*)
 					echo "Unknown option";
@@ -38,6 +47,12 @@ while getopts "$optspec" optchar; do
 done
 shift "$((OPTIND-1))"
 
+# if --btos check if date is set
+if [[ $SYNC_DIRECTION == 'btos' ]] && [[ -z $DATE || ! $DATE =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}$ ]]; then
+	echo "Date format is not correct. Correct format is: yyyy-mm-dd"
+	exit 1;
+fi
+
 if [[ -z $1 ]]; then
 	echo "missing configuration"
 	exit 1
@@ -46,18 +61,18 @@ fi
 source $1
 source "functions.sh"
 
-if [[ $DIRECTION == 'ltos' ]]; then
-	SRC=$LOCAL_DATA_TARGET/
+if [[ $SYNC_DIRECTION == 'ltos' ]]; then
+	SOURCE_PATH=$LOCAL_DATA_TARGET/
 	TARGET="${SSHUSERNAME}@${SERVERHOST}:${REMOTESRC}"
-elif [[ $DIRECTION == 'stol' ]]; then
-	SRC="${SSHUSERNAME}@${SERVERHOST}:${REMOTESRC}/"
+elif [[ $SYNC_DIRECTION == 'stol' ]]; then
+	SOURCE_PATH="${SSHUSERNAME}@${SERVERHOST}:${REMOTESRC}/"
 	TARGET=$LOCAL_DATA_TARGET
-elif [[ $DIRECTION == 'btos' ]]; then
+elif [[ $SYNC_DIRECTION == 'btos' ]]; then
 	echo "Date : $DATE";
-	SRC="${BASEBACKUPFOLDER}/${SERVERNAME}/DATA/$DATE/"
+	SOURCE_PATH="${BASEBACKUPFOLDER}/${SERVERNAME}/DATA/$DATE/"
 	TARGET="${SSHUSERNAME}@${SERVERHOST}:${REMOTESRC}"
 else
-	echo "unknown direction!"
+	echo "unknown SYNC_DIRECTION!"
 	exit 0;
 fi
 
@@ -73,4 +88,4 @@ then
 	done
 fi
 
-rsync $OPT -e "$RSNYCSHELL" $SRC $TARGET
+rsync $OPT -e "$RSNYCSHELL" $SOURCE_PATH $TARGET
